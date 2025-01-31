7fd47f9982d548541bada0ea932bf28d6cf44332
cevikbasesra <cevikbasesra@gmail.com>
2025-01-31 20:00:04 +0300

    s15d1 solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        /*Grocery grocery = new Grocery();
+        grocery.startGrocery();*/
+
+        MobilePhone mobilePhone = new MobilePhone("1212121212", new ArrayList<>());
+        mobilePhone.addNewContact(new Contact("esra","1313"));
+        mobilePhone.addNewContact(new Contact("ali","1414"));
+        mobilePhone.printContacts();
     }
 }
